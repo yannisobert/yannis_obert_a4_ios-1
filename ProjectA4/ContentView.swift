562367d5d7fedc525edc772270b9797bb0be66b1
yannisobert <90550556+yannisobert@users.noreply.github.com>
2023-11-20 11:26:49 +0100

    Initial Commit

---
+++ ProjectA4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjectA4
+//
+//  Created by Yannis Obert on 20/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProjectA4/ProjectA4App.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectA4App.swift
+//  ProjectA4
+//
+//  Created by Yannis Obert on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProjectA4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
